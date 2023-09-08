5382869c9db7391e096abbbce6730a81d5c141a9
Purushotham230898 <raopurushotham257@gmail.com>
2023-09-08 13:48:19 +0530

    document changes

---
+++ index.js
@@ -0,0 +1,7 @@
+let item = document.getElementsByClassName("list-group-item");
+//console.log(items);
+item[2].style.backgroundColor ='green';
+item[0].style.fontWeight ='bold';
+item[1].style.fontWeight ='bold';
+item[2].style.fontWeight ='bold';
+item[3].style.fontWeight ='bold';
