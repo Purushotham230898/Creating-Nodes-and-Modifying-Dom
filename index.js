2092797667aedb9f97b9008719b224cd0a875ef4
Mprao230898 <majjipurushotham98@gmail.com>
2023-09-09 11:46:56 +0530

    first commit

--- index.js
+++
@@ -1,7 +0,0 @@
-let item = document.getElementsByClassName("list-group-item");
-//console.log(items);
-item[2].style.backgroundColor ='green';
-item[0].style.fontWeight ='bold';
-item[1].style.fontWeight ='bold';
-item[2].style.fontWeight ='bold';
-item[3].style.fontWeight ='bold';
