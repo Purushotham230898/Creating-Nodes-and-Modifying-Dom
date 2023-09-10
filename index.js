e1cd068f20f7b502a4098c676724df6311f43c2c
Mprao230898 <majjipurushotham98@gmail.com>
2023-09-10 23:53:49 +0530

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor="green";
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display="none";
+var thirdItem = document.querySelectorAll('li:nth-child(2)');
+thirdItem[0].style.backgroundColor = 'green';
+
+
+var odd=document.querySelectorAll("li:nth-child(odd)");
+for(var i=0;i<odd.length;i++)
+{
+  odd[i].style.backgroundColor="green";
+}
+
+
 
