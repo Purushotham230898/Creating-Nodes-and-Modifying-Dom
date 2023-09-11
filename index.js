010638548592f74fa02829c87e516ebce19f2374
Mprao230898 <majjipurushotham98@gmail.com>
2023-09-11 11:51:38 +0530

    Modifying DOM

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
+var itemList = document.querySelector('#items');
 
-var thirdItem = document.querySelectorAll('li:nth-child(2)');
-thirdItem[0].style.backgroundColor = 'green';
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='yellow';
 
+console.log(itemList.firstChild);
 
-var odd=document.querySelectorAll("li:nth-child(odd)");
-for(var i=0;i<odd.length;i++)
-{
-  odd[i].style.backgroundColor="green";
-}
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent='I LOVE THIS';
 
+console.log(itemList.lastChild);
 
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent='I LOVE THIS MORE';
 
+console.log(itemList.childNodes);
+
+console.log(itemList.nextSibling);
+
+console.log(itemList.nextElementSibling);
+
+console.log(itemList.previousSibling);
+
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color='red';
+
+//Creating a element
+
+var newDiv=document.createElement('div');
+newDiv.className='First';
+newDiv.id='Second';
+newDiv.setAttribute('title','hello First&Second');
+
+var newDivText=document.createTextNode('HEllo word');
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+console.log(newDiv);
+container.insertBefore (newDiv, h1);
+
+var itemList = document.getElementById("items");
+var newItem = document.createElement("li");
+newItem.className = "list-group-item";
+newItem.textContent = "Hello World";
+var firstItem = itemList.querySelector("li");
+itemList.insertBefore(newItem, firstItem);
